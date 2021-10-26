1c6d6b8b40a76ae71cbe935a732a937705a34de6
Ben Kasper <bckasper@gmail.com>
2021-10-26 14:59:53 -0400

    added the POST route for the tag-routes.js file

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,8 +38,19 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try{
+    const newTag = await Tag.create(req.body);
+
+    if(!newTag){
+      res.status(404).json({message: 'Please enter a valid input for a new tag!'})
+    }
+
+    res.status(200).json(newTag);
+  } catch (err){
+    res.status(400).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
