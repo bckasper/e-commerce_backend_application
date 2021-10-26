87519f4f2cd2920a33b17b2b67f87f13cb965127
Ben Kasper <bckasper@gmail.com>
2021-10-26 18:03:28 -0400

    added the PUT and DELETE routes for the tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -53,12 +53,46 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try{
+    const updatedTag = await Tag.update(
+      {
+        tag_name: req.body.tag_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+      );
+
+      if(!updatedTag){
+        res.status(404).json({message: 'There is no tag with that ID. Try again!'})
+      }
+
+      res.status(200).json(updatedTag);
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try{
+    const deletedTag = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+
+    if(!deletedTag){
+      res.status(404).json({message: 'There is no tag with that ID. Try again!'})
+    }
+    res.status(200).json(deletedTag)
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 module.exports = router;
